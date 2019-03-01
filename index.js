fc221f33f5b78e00a06e3d3955bc44249a2781b0
Breci <brice.culas@edu.gobelins.fr>
2019-03-01 19:47:11 +0100

    init

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-}
+module.exports = {
+    sayHelloInEnglish: function() {
+        return "HELLO";
+    },
+
+    sayHelloInSpanish: function() {
+        return "Hola";
+    }
+};
